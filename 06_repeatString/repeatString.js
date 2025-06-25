19498bea0c0b0c3a405f61c5df34260f6f76a332
Waris Sripatoomrak <167295008+waris-sri@users.noreply.github.com>
2025-06-25 23:12:00 +0700

    feat: complete exercise 01-06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (s, n) {
+  if (n < 0) return "ERROR";
+  let str = "";
+  for (let i = 0; i < n; i++) {
+    str += s;
+  }
+  return str;
 };
+// repeatString("hey", 3);
 
 // Do not edit below this line
 module.exports = repeatString;
